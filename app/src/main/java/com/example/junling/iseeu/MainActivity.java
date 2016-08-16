27b41d31c0622cc6268528a4108b3f75cfa79c3a
Liu Junling <liujunling1202@gmail.com>
2016-08-16 17:00:34 +0800

    homepage

--- app/src/main/java/com/example/junling/iseeu/MainActivity.java
+++ app/src/main/java/com/example/junling/iseeu/MainActivity.java
@@ -8,13 +8,17 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Boolean loginOrNot = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
