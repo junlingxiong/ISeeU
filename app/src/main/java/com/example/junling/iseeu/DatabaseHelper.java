ce43f87821756efdbf4e9b0cdad5746578959610
Liu Junling <liujunling1202@gmail.com>
2016-08-19 13:42:22 +0800

    database set up

---
+++ app/src/main/java/com/example/junling/iseeu/DatabaseHelper.java
@@ -0,0 +1,64 @@
+package com.example.junling.iseeu;
+
+/**
+ * Created by Junling on 19/8/16.
+ */
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import com.example.junling.iseeu.entitys.Tablet;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    // Logcat tag
+    private static final String LOG = "DatabaseHelper";
+    // Database Version
+    private static final int DATABASE_VERSION = 1;
+    // Database Name
+    private static final String DATABASE_NAME = "ISeeUDB";
+
+    // Table Names
+    private static final String TABLE_TABLET = "tablet";
+
+    //Tablet Column names 
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        // creating required tables
+        //db.execSQL(CREATE_TABLE_TABLET);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // on upgrade drop older tables
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_TABLET);
+        // create new tables
+        onCreate(db);
+    }
+
+    // closing database
+    public void closeDB() {
+        SQLiteDatabase db = this.getReadableDatabase();
+        if (db != null && db.isOpen())
+            db.close();
+    }
+
+    //helpful DB tutorial https://developer.android.com/training/basics/data-storage/databases.html#DefineContract
+}
---
+++ app/src/main/java/com/example/junling/iseeu/entitys/Tablet.java
@@ -0,0 +1,13 @@
+package com.example.junling.iseeu.entitys;
+
+/**
+ * Created by Junling on 19/8/16.
+ */
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.UUID;
+
+public class Tablet implements Serializable{
+
+}
