e5067f21c66c128a4250a6e43802699b1a96b475
Liu Junling <liujunling1202@gmail.com>
2016-09-01 16:26:31 +0800

    update

--- app/src/main/java/com/example/junling/iseeu/tablet/RegisterActivity.java
+++ app/src/main/java/com/example/junling/iseeu/tablet/RegisterActivity.java
@@ -58,40 +58,6 @@ public class RegisterActivity extends AppCompatActivity {
             finish();
         }
 
-        /*
-        ConnectivityManager connMgr = (ConnectivityManager)
-                getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
-        if (networkInfo != null && networkInfo.isConnected()) {
-            String deviceName = ((EditText) findViewById(R.id.device_name)).getText().toString();
-
-            // store the device name and password into persistent storage, after which retrieval is ready for video call
-            SharedPreferences sharedPreferences = getSharedPreferences(Constants.TABLET_SHARED_PREFERENCES, Context.MODE_PRIVATE);
-            SharedPreferences.Editor editor = sharedPreferences.edit();
-            editor.putString(Constants.KEY_DEVICE_NAME, deviceName);
-
-            editor.commit();
-            // display registration success dialogue
-            new AlertDialog.Builder(this)
-                    .setTitle("Registration Successful")
-                    .setMessage("Device Name: " + deviceName + "\n")
-                    .setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                        public void onClick(DialogInterface dialog, int which) {
-                            startActivity(new Intent(RegisterActivity.this, PrivacyModeActivity.class));
-                            finish();
-                        }
-                    })
-                    .setNegativeButton("Reset", new DialogInterface.OnClickListener() {
-                        public void onClick(DialogInterface dialog, int which) {
-                            startActivity(new Intent(RegisterActivity.this, ResetActivity.class));
-                            finish();
-                        }
-                    })
-                    .setIcon(android.R.drawable.ic_dialog_info)
-                    .show();
-        } else {
-            Toast.makeText(this, "Please enable WiFi or cellular data to video-chat!", Toast.LENGTH_SHORT).show();
-        }*/
     }
 
     private void showAlertDialog_device(){
